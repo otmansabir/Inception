4613a5f7443aaa9282b3341d52195c71b2309ff9
Otman Sabir <osabir@e1r7p5.1337.ma>
2024-12-14 12:35:32 +0100

    check everthing before push to the 42inter

--- srcs/requirements/mariadb/tools/scrpt.bash
+++ srcs/requirements/mariadb/tools/scrpt.bash
@@ -4,15 +4,14 @@ service mariadb start
 
 sleep 5
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MARIA_DABE}\`;"
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS \`${MARIA_DABE}\`;"
 
-mariadb -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mariadb -u root -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
 
-mariadb -e "GRANT ALL PRIVILEGES ON ${MARIA_DABE}.* TO \`${MARIADB_USER}\`@'%';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON \`${MARIA_DABE}\`.* TO \`${MARIADB_USER}\`@'%';"
 
-mariadb -e "FLUSH PRIVILEGES;"
+mariadb -u root -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$MARIADB_PASSWORD shutdown
+mysqladmin -u root -p"${MARIADB_PASSWORD}" shutdown
 
 mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
-
